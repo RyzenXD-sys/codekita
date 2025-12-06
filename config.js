88bb9240d08ac1c03a41dd665ade5892a0ee064c
teamhookrest-prog <teamhookrest@gmail.com>
2025-12-06 18:25:53 +0700

    Upload file: config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  GITHUB_USERNAME: process.env.GITHUB_USERNAME || "your-github-username",
+  GITHUB_REPO: process.env.GITHUB_REPO || "your-repo-name",
+  GITHUB_TOKEN: process.env.GITHUB_TOKEN || "your-github-token",
+}
